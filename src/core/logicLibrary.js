e99e0336fe4b52681f2a2b8e4ffbc954203b3a1d
Matraellix <maelle.fleitz.pro@gmail.com>
2025-02-07 17:31:12 +0100

    Added some logic functions for futur scroll animation

--- src/core/logicLibrary.js
+++ src/core/logicLibrary.js
@@ -1,5 +1,7 @@
 function lerp(start, end, t) {
     return start * (1 - t) + end * t;
 }
-
-export {lerp};
+function scalePercent(start, end) {
+    return Math.max(0, Math.min(1, (scrollPercent - start) / (end - start)));
+}
+export {lerp,scalePercent};
